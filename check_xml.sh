3845216baa74ff9ee0edbeae342755d06f1f0c75
redmaner <jakevdputten@gmail.com>
2014-08-16 17:31:17 +0200

    4.2: Integrate caching into check_xml

--- check_xml.sh
+++ check_xml.sh
@@ -10,6 +10,37 @@ XML_CACHE_LOG=$CACHE/XML_CACHE_LOG
 XML_LOG_TEMP=$CACHE/XML_LOG_TEMP
 
 #########################################################################################################
+# CACHING
+#########################################################################################################
+build_cache () {
+if [ -d $CACHE ]; then
+	case "$SERVER" in
+		yes) rm -rf $CACHE; mkdir $CACHE;;
+		 no) echo -e "${txtred}ERROR:${TXTRST} $CACHE already exsists\nDo you want to remove the cache? This can interrupt a current check!"
+	    	     echo -en "(y,n): "; read cache_remove_awnser
+		     if [ $cache_remove_awnser == "y" ]; then
+				rm -rf $CACHE; mkdir $CACHE
+		     else
+				exit
+		     fi;;
+	esac
+else
+	rm -rf $CACHE; mkdir $CACHE
+fi
+}
+
+clear_cache () {
+rm -rf $CACHE
+}
+
+clean_cache () {
+rm -f $XML_TARGETS_STRIPPED
+rm -f $DOUBLE_RESULT
+rm -f $OPOSTROPHE_RESULT
+rm -f $XML_CACHE_LOG
+}
+
+#########################################################################################################
 # INITIAL LOGGING
 #########################################################################################################
 # Define logs
