65c7e681a32e41ac97c522c661dd510bd4f63743
Redmaner <jakevdputten@gmail.com>
2016-02-07 19:03:19 +0100

    Fix some html issues

--- create_log.sh
+++ create_log.sh
@@ -165,37 +165,37 @@ a:hover {
 <br><br>
 <table border="0" cellpadding="0" cellspacing="0">
 	<tr>
-		<td height="auto" width="17%"><span class="green">Green text</span></td>
-		<td height="auto" width="auto"><span class="black">No errors found</span></td>
+		<td height="auto" width="120px"><span class="green">Green text</span></td>
+		<td height="auto" width="auto"><span class="black">No errors found</span><td>
 	</tr>
 	<tr>
-		<td height="auto" width="17%"><span class="red">Red text</span></td>
-		<td height="auto" width="auto"><span class="black">Parser error [Found in $COUNT_RED file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="red">Red text</span></td>
+		<td height="auto" width="auto"><span class="black">Parser error [Found in $COUNT_RED file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width='17%"><span class="orange">Orange text</span></td>
-		<td height="auto" width="auto"><span class="black">Double strings [Found in $COUNT_ORANGE file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="orange">Orange text</span></td>
+		<td height="auto" width="auto"><span class="black">Double strings [Found in $COUNT_ORANGE file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width="17%"><span class="brown">Brown text</span></td>
-		<td height="auto" width="auto"><span class="black">Apostrophe syntax error  [Found in $COUNT_BROWN file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="brown">Brown text</span></td>
+		<td height="auto" width="auto"><span class="black">Apostrophe syntax error  [Found in $COUNT_BROWN file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width="17%"><span class="pink">Pink text</span></td>
-		<td height="auto" width="auto"><span class="black">Untranslateable string, array or plural - Has to be removed from xml!  [Found in $COUNT_PINK file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="pink">Pink text</span></td>
+		<td height="auto" width="auto"><span class="black">Untranslateable string, array or plural - Has to be removed from xml!  [Found in $COUNT_PINK file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width="17%"><span class="cyan">Cyan text</span></td>
-		<td height="auto" width="auto"><span class="black">Wrong values folder  [Found in $COUNT_CYAN file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="cyan">Cyan text</span></td>
+		<td height="auto" width="auto"><span class="black">Wrong values folder  [Found in $COUNT_CYAN file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width="17%"><span class="blue">Blue text</span></td>
-		<td height="auto" width="auto"><span class="black">'+' outside of tags  [Found in $COUNT_BLUE file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="blue">Blue text</span></td>
+		<td height="auto" width="auto"><span class="black">'+' outside of tags  [Found in $COUNT_BLUE file(s)]</span></td><td>
+	</td></tr>
 	<tr>
-		<td height="auto" width="17%"><span class="grey">Grey text</span></td>
-		<td height="auto" width="auto"><span class="black">Invalid variable formatting  [Found in $COUNT_GREY file(s)]</span></td>
-	</tr>
+		<td height="auto" width="120px"><span class="grey">Grey text</span></td>
+		<td height="auto" width="auto"><span class="black">Invalid variable formatting  [Found in $COUNT_GREY file(s)]</span></td><td>
+	</td></tr>
 </table>
 EOF
 }
